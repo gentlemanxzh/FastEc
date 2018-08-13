688fb7303f9a0434573d744690d163dcd0378843
Gogs <gogs@fake.local>
2018-08-13 23:00:13 +0800

    个人信息更新实现2

--- latte-ui/src/main/java/com/example/ui/camera/CameraHandler.java
+++ latte-ui/src/main/java/com/example/ui/camera/CameraHandler.java
@@ -1,5 +1,10 @@
 package com.example.ui.camera;
 
+import android.app.AlertDialog;
+import android.support.v4.content.PermissionChecker;
+
+import com.example.latter.delegates.PermissionCheckerDelegate;
+
 /**
  * @author gentleman
  * @date 2018/8/9
@@ -8,4 +13,16 @@ package com.example.ui.camera;
 
 public class CameraHandler {
 
+    private final AlertDialog DIALOG;
+    private final PermissionCheckerDelegate DELEGATE;
+
+    public CameraHandler(AlertDialog dialog,PermissionCheckerDelegate delegate){
+        this.DIALOG = dialog;
+        this.DELEGATE = delegate;
+    }
+
+    final void beginCameraDialog(){
+        DIALOG.show();
+        
+    }
 }
